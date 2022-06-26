11c8a9547de54b85f35a4f2acc58473b511f51e0
Che-Yu Wu <frank890417@gmail.com>
2022-06-26 07:27:52 -0400

    update task board

--- frontend/src/components/Taskboard.jsx
+++ frontend/src/components/Taskboard.jsx
@@ -1,21 +1,56 @@
-import { Box, Container, AccordionItem, Accordion, AccordionButton, AccordionIcon, AccordionPanel, Text, Image, Button, Flex, Progress, Spacer, Center, Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import { Box, Input, Container, AccordionItem, Accordion, AccordionButton, AccordionIcon, AccordionPanel, Text, Image, Button, Flex, Progress, Spacer, Center, Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
 import React from "react";
-import { LockIcon } from "@chakra-ui/icons"
+import { LockIcon, StarIcon } from "@chakra-ui/icons"
 import { Fade } from '@chakra-ui/react'
 let questions = [
     {
         id: 0,
-        question: "question 1",
-        answer: "answer 1",
+        question: "Question 1",
+        solved: true,
+        answers: [
+            {
+                id: 0,
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                bestAnswer: true
+            }, {
+                id: 1,
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                bestAnswer: false
+            }
+        ],
+        reward: 3,
+        rewardUnit: "LLL"
+    }, {
+        id: 0,
+        question: "Question 2",
+        solved: false,
+        answers: [
+            {
+                id: 0,
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                bestAnswer: false
+            }, {
+                id: 1,
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                bestAnswer: false
+            }
+        ],
         reward: 3,
         rewardUnit: "LLL"
     }
 ]
 
 export const Taskboard = ({ course, handleBack, handlePurchase, }) => {
+    let handleAddQuestion = () => {
+
+    }
     return <Container maxW="container.lg" minH="100vh">
-        <Accordion defaultIndex={ [0] } allowMultiple>
-            { questions.map(({ question, answer, reward, rewardUnit }, i) => <AccordionItem>
+        <Flex w="100%">
+            <Input placeholder="ask something"></Input>
+            <Button onClick={ handleAddQuestion() }>Ask Question</Button>
+        </Flex>
+        <Accordion mt={ 2 } defaultIndex={ Array.from({ length: questions.length }).map((d, i) => i) } allowMultiple>
+            { questions.map(({ question, answers, reward, rewardUnit }, i) => <AccordionItem>
                 <Box backgroundColor="white" shadow="xl" borderRadius="50" mt="1" overflow="hidden">
                     <AccordionButton >
                         <Box flex='1' textAlign='left' p={ 1 }>
@@ -24,18 +59,38 @@ export const Taskboard = ({ course, handleBack, handlePurchase, }) => {
                                     { question }
                                 </Text>
                                 <Spacer />
-                                <Text>$ { reward } { rewardUnit }</Text>
+                                <Text> { question.solved && '[Solved]' } </Text>
+                                <Text> $ { reward } { rewardUnit }</Text>
                             </Flex>
 
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                 </Box>
-                <AccordionPanel pb={ 4 }>
-                    { answer }
+                <AccordionPanel pb={ 4 } pt="10">
+                    { answers.map(answer =>
+                        <Box flex='1' textAlign='left' p={ 3 } backgroundColor={ answer.bestAnswer ? 'yellow.300' : '' }>
+                            <Flex>
+                                <Text flex="6">
+                                    { answer.bestAnswer && <StarIcon mr={ 2 }></StarIcon> }
+                                    { answer.content }
+
+                                </Text>
+                                <Spacer></Spacer>
+                                { !answer.bestAnswer && <Button borderRadius="50" >
+                                    <StarIcon></StarIcon>
+                                </Button> }
+                            </Flex>
+                        </Box>
+                    ) }
+
+                    <Flex w="100%">
+                        <Input placeholder="Answer Question"></Input>
+                        <Button onClick={ answerQuestion() }>Answer</Button>
+                    </Flex>
                 </AccordionPanel>
             </AccordionItem>) }
         </Accordion>
-    </Container>
+    </Container >
 }
 

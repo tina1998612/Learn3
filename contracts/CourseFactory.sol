9e38936b3fb8440d0f73dda5abe31851682d1967
tina1998612 <tleeae@connect.ust.hk>
2022-06-25 23:40:04 -0400

    update contract

---
+++ contracts/CourseFactory.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Course.sol";
+
+contract CourseFactory is Ownable {
+    Course[] public courses;
+
+    function createCourse(
+        string memory _name, 
+        string memory _symbol,
+        uint _price,
+        string memory _baseTokenURI,
+        bool _isCrowdfund,
+        uint _crowdfundPeriod,
+        uint _crowdfundGoalStudentCount,
+        uint _refundPeriod,
+        address[] memory _tutors,
+        uint[] memory _tutorsPercent,
+        uint _QnABoardShare
+    ) external returns (address) {
+        Course c = new Course(_name, _symbol, _price, _baseTokenURI, _isCrowdfund, _crowdfundPeriod, _crowdfundGoalStudentCount, _refundPeriod, _tutors, _tutorsPercent, _QnABoardShare);
+        courses.push(c);
+        return address(c);
+    }
+
+    function withdraw() external onlyOwner {
+        Address.sendValue(payable(owner()), address(this).balance);
+    }
+}
